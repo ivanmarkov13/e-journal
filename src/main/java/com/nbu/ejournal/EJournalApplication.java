90ee34652b0e6c223da14420f1334c7504b768eb
Ivan Markov <Ivan_Markov@epam.com>
2019-11-20 03:34:45 +0200

    initial Spring initializer setup

---
+++ src/main/java/com/nbu/ejournal/EJournalApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.ejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbu/ejournal/EJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.ejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
